12ceba0aba3a44816560258b1527be457197c1e9
Daniel Pereira <dtvp@cesar.org.br>
2014-01-30 09:05:57 -0200

    Adicionado o projeto Servo PWM. Controlando os servos pelo timer

---
+++ 6-ServoPWM/main.c
@@ -0,0 +1,158 @@
+#include "misc.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_rcc.h"
+#include "stm32f4xx_tim.h"
+
+
+TIM_TimeBaseInitTypeDef		TIM_TimeBaseStructure;
+TIM_OCInitTypeDef			TIM_OCInitStructure;
+
+__IO uint16_t Servo1Pulse = 1000;		// 1000 us = 1 ms
+__IO uint16_t Servo2Pulse = 2000;		// 2000 us = 2 ms
+
+
+void TIM_ConfigPins(void);
+
+
+int main(void)
+{
+
+	SystemInit();
+
+	/* TIM1 Configuration */
+	TIM_ConfigPins();
+
+	/* Time Base configuration */
+	// TIM4_CNT increments every 1 us
+	TIM_TimeBaseStructure.TIM_Prescaler = (uint16_t) ( ( (SystemCoreClock / 2) / 1000000) - 1);
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+	// TIM4_CNT overflows every 20 ms
+	TIM_TimeBaseStructure.TIM_Period = 20000;
+	TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+	TIM_TimeBaseStructure.TIM_RepetitionCounter = 0;
+
+	TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure);
+
+
+	/* Channel 1 to 4 Configuration in Output Compare Toggle mode */
+	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+	TIM_OCInitStructure.TIM_Pulse = Servo1Pulse;
+	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+
+	TIM_OC1Init(TIM4, &TIM_OCInitStructure);
+	TIM_OC2Init(TIM4, &TIM_OCInitStructure);
+
+	TIM_OCInitStructure.TIM_Pulse = Servo2Pulse;
+	TIM_OC3Init(TIM4, &TIM_OCInitStructure);
+	TIM_OC4Init(TIM4, &TIM_OCInitStructure);
+
+	/* TIM1 counter enable */
+	TIM_Cmd(TIM4, ENABLE);
+
+	/* TIM IT enable */
+	TIM_ITConfig(TIM4, TIM_IT_Update | TIM_IT_CC1 | TIM_IT_CC2 | TIM_IT_CC3 | TIM_IT_CC4, ENABLE);
+
+	while (1)
+	{
+	}
+}
+
+
+/**
+ * @brief  This function handles TIM4 global interrupt request.
+ * @param  None
+ * @retval None
+ */
+void TIM4_IRQHandler(void)
+{
+	uint16_t capture = 0;
+
+	/* TIM4_CH1 toggling */
+	if (TIM_GetITStatus(TIM4, TIM_IT_CC1) != RESET)
+	{
+		TIM_ClearITPendingBit(TIM4, TIM_IT_CC1 );
+		capture = TIM_GetCapture1(TIM4);
+		TIM_SetCompare1(TIM4, capture + Servo1Pulse );
+	}
+
+	/* TIM4_CH2 toggling */
+	if (TIM_GetITStatus(TIM4, TIM_IT_CC2) != RESET)
+	{
+		TIM_ClearITPendingBit(TIM4, TIM_IT_CC2);
+		capture = TIM_GetCapture2(TIM4);
+		TIM_SetCompare2(TIM4, capture + Servo1Pulse);
+	}
+
+	/* TIM4_CH3 toggling */
+	if (TIM_GetITStatus(TIM4, TIM_IT_CC3) != RESET)
+	{
+		TIM_ClearITPendingBit(TIM4, TIM_IT_CC3);
+		capture = TIM_GetCapture3(TIM4);
+		TIM_SetCompare3(TIM4, capture + Servo2Pulse);
+	}
+
+	/* TIM4_CH4 toggling */
+	if (TIM_GetITStatus(TIM4, TIM_IT_CC4) != RESET)
+	{
+		TIM_ClearITPendingBit(TIM4, TIM_IT_CC4);
+		capture = TIM_GetCapture4(TIM4);
+		TIM_SetCompare4(TIM4, capture + Servo2Pulse);
+	}
+
+	/* TIM_Update reset all */
+	if (TIM_GetITStatus(TIM4, TIM_IT_Update) != RESET)
+	{
+		TIM_ClearITPendingBit(TIM4, TIM_IT_Update);
+		TIM_SetCompare1(TIM4, Servo1Pulse);
+		TIM_SetCompare2(TIM4, Servo2Pulse);
+	}
+
+
+}
+
+
+
+
+/**
+ * @brief  Configure the TIM4 Pins.
+ * @param  None
+ * @retval None
+ */
+void TIM_ConfigPins(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	NVIC_InitTypeDef NVIC_InitStructure;
+
+	/* GPIOD clock enable */
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+
+	/* TIM4 clock enable */
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+
+	/* GPIOD Configuration: Channels 1 to 4 as alternate function push-pull */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+	/* Connect TIM pins to AF1 */
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource12, GPIO_AF_TIM4);
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource13, GPIO_AF_TIM4);
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource14, GPIO_AF_TIM4);
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource15, GPIO_AF_TIM4);
+
+	/* Enable the TIM4 global Interrupt */
+	NVIC_InitStructure.NVIC_IRQChannel = TIM4_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+
+}
+
+
+
+
